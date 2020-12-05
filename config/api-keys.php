a9169e53d8966eebf95180f6d4cf9288b65b48e1
Olof Johansson <olaf.sebastian.johansson@gmail.com>
2020-12-05 18:50:41 +0100

    Enable use of environment variables in Travis CI

--- config/api-keys.php
+++ config/api-keys.php
@@ -2,6 +2,13 @@
 /**
  * Configuration file.
  */
+if (getenv("MY_TRAVIS_ENV")) {
+    return [
+        "geotag-key" => getenv("GEOTAG_KEY"),
+        "weather-key" => getenv("WEATHER_KEY"),
+    ];
+}
+
 return [
     "geotag-key" => file_get_contents(ANAX_INSTALL_PATH . "/data/GEOTAG_KEY"),
     "weather-key" => file_get_contents(ANAX_INSTALL_PATH . "/data/WEATHER_KEY"),
